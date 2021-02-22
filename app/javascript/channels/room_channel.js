6346135b41fc29a20b534c958eb3d6e97e3a7a9f
kohei <kouhik06@gmail.com>
2021-02-23 03:58:45 +0900

    簡易的なチャットアプリ作成 rails g job MessageBroadcast実行後に編集

--- app/javascript/channels/room_channel.js
+++ app/javascript/channels/room_channel.js
@@ -10,7 +10,8 @@ const appRoom = consumer.subscriptions.create("RoomChannel", {
   },
 
   received(data) {
-    return alert(data['message']);
+    const messages = document.getElementById('messages');
+    messages.insertAdjacentHTML('beforeend', data['message']);
   },
 
   speak: function(message) {
